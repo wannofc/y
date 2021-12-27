da93dc40c3821387b95f800064f22b3083a610fc
viko-api <vikodwik@gmail.com>
2021-12-27 18:23:17 +0700

    first commit

---
+++ routes/api.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const { cekKey } = require('../database/db'); 
+const { youtubePlay, youtubeMp4, youtubeMp3 } = require('../controllers/yt');
+const { cakLontong, bijak, quotes, fakta, ptl, motivasi } = require('../controllers/randomtext');
+
+router.get('/cekapi', async (req, res) => {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} tidak ditemukan, silahkan anda login/register untuk mendapatkan api vikoapi-index.herokuapp.com`
+    });
+    res.send({status: 200, apikey: apikey, limit: '900 limit', note: 'apikey aktif, silahkan gunakan restapinya'});
+});
+
+router.get('/ytplay', youtubePlay);
+
+router.get('/ytmp4', youtubeMp4);
+
+router.get('/ytmp3', youtubeMp3);
+
+router.get('/caklontong', cakLontong);
+
+router.get('/quotes', quotes);
+
+router.get('/fakta', fakta);
+
+router.get('/bijak', bijak);
+
+router.get('/ptl', ptl);
+
+router.get('/motivasi', motivasi);
+
+module.exports = router;
